67bcc25e3dfdfac369212a52045778d3ae9a55c1
JacobGurley <united.states.productions@gmail.com>
2024-03-03 21:27:27 -0800

    Set variable for debug

--- app.py
+++ app.py
@@ -190,5 +190,6 @@ def remove_duplicate(playlist_id, track_id):
         response = {"status": "error", "message": f"Failed to remove track. Error: {str(e)}"}
         return jsonify(response), 500
 
+debug_mode = os.environ.get('FLASK_DEBUG', 'False') == 'True'
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=debug_mode, host='0.0.0.0', port=5000)
